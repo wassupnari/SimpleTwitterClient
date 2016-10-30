26d3ef78fe8e00eb173487cc2d6052678ff46321
wassupnari <wassupnari@gmail.com>
2016-10-30 11:07:07 -0700

    Fixed small issue with userDefaults

--- SimpleTwitterClient/User.swift
+++ SimpleTwitterClient/User.swift
@@ -14,8 +14,12 @@ class User: NSObject {
     var screenname: String?
     var profileUrl: URL?
     var tagline: String?
+    
+    var dictionary: NSDictionary?
 
     init(dictionary: NSDictionary) {
+        self.dictionary = dictionary
+        
         name = dictionary["name"] as? String
         screenname = dictionary["screen_name"] as? String
         let profileUrlString = dictionary["profile_image_url_https"] as? String
@@ -24,5 +28,42 @@ class User: NSObject {
             profileUrl = URL(string: profileUrlString)
         }
         tagline = dictionary["description"] as? String
+    }
+    
+    static var _currentUser: User?
+    
+    class var currentUser: User? {
+        // Create computed properties
+        get {
+            
+            if _currentUser == nil {
+            
+                let defaults = UserDefaults.standard
+            
+                let userData = defaults.object(forKey: "currentUserData") as? Data
+        
+                if let userData = userData {
+                    let dictionary = try! JSONSerialization.data(withJSONObject: userData, options: []) as! NSDictionary
+                
+                    _currentUser = User(dictionary: dictionary)
+                
+                }
+            }
+            return _currentUser
+        }
+        
+        set(user) {
+            let defaults = UserDefaults.standard
+            
+            if let user = user {
+                let data = try! JSONSerialization.data(withJSONObject: user.dictionary, options: [])
+                defaults.set(data, forKey: "currentUserData")
+                
+            } else {
+                defaults.set(nil, forKey: "currentUserData")
+            }
+            
+            defaults.synchronize()
+        }
     }
 }
