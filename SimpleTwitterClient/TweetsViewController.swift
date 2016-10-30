f4c9a7f4158eea1c5bd528c4063bc8486cd4194a
wassupnari <wassupnari@gmail.com>
2016-10-30 16:35:57 -0700

    Added refresh control

--- SimpleTwitterClient/TweetsViewController.swift
+++ SimpleTwitterClient/TweetsViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class TweetsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    let alertController = UIAlertController(title: nil, message: "Please wait\n\n", preferredStyle: UIAlertControllerStyle.alert)
+    let errorAlertController = UIAlertController(title: "Error", message: "An error occurred. Please try it again", preferredStyle: UIAlertControllerStyle.alert)
+
 
     var tweets = [Tweet]()
     override func viewDidLoad() {
@@ -21,15 +24,12 @@ class TweetsViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 160
         
-        TwitterClient.sharedInstance?.homeTimeline(success: { (tweets: [Tweet]) in
-            self.tweets = tweets
-            self.tableView.reloadData()
-            }, failure: { (error: Error) in
-                print("error: \(error.localizedDescription)")
-        })
-
-        // Do any additional setup after loading the view.
+        getTweets(fromRefresh: false, success: nil, failure: nil)
         
+        // For pull-to-refresh
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(TweetsViewController.refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         
     }
 
@@ -50,6 +50,34 @@ class TweetsViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tweets.count ?? 0
+    }
+    
+    func getTweets(fromRefresh: Bool, success: (() -> ())?, failure: ((Error) -> ())?) {
+        TwitterClient.sharedInstance?.homeTimeline(success: { (tweets: [Tweet]) in
+            self.tweets = tweets
+            self.tableView.reloadData()
+            if fromRefresh {
+                success!()
+            }
+            }, failure: { (error: Error) in
+                if fromRefresh {
+                    failure!(error)
+                }
+                print("error: \(error.localizedDescription)")
+        })
+    }
+    
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        
+        self.present(alertController, animated: false, completion: nil)
+        
+        getTweets(fromRefresh: true, success: {
+            self.dismiss(animated: false, completion: nil)
+            refreshControl.endRefreshing()
+        }) { (error: Error) in
+                // Do nothing
+        }
+        
     }
 
     /*
